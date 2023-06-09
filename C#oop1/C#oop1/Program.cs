da29c1f4b7df9bd68d6ec91b99287dc9be740359
SAIMAALI\LENOVO <saima.miti7@gmail.com>
2023-06-09 15:26:58 +0600

    INITIAL COMMIT

---
+++ C#oop1/C#oop1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace C_oop1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, World!");
+            car bmw = new car();
+            bmw.Start();
+            bmw.color = Color.Red;
+            Console.WriteLine(bmw.color);
+            bmw.Speed();
+
+            car audi = new car();
+            audi.Start();
+            audi.Speed();
+        }
+    }
+}
